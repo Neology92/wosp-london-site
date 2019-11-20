939f2e736e8f197cafc9552cd2ba062f4fe2dfa6
Neology92 <oskarlegner@gmail.com>
2019-11-20 14:32:08 +0100

    New commit

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,5 +4,6 @@ import Dunno from './Dunno';
 import Header from './Header/Header';
 import Footer from './Footer';
 import Slider from './Slider';
+import BootstrapTest from './BootstrapTest';
 
-export { Logo, Cheese, Dunno, Footer, Header, Slider };
+export { Logo, Cheese, Dunno, Footer, Header, Slider, BootstrapTest };
--- src/Layouts/Home.js
+++ src/Layouts/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import styled from 'styled-components';
 import { Col, Row, Container } from 'react-bootstrap';
-import { Slider } from '../Components';
+import { Slider, BootstrapTest } from '../Components';
 
 const Home = () => {
     return (
@@ -9,6 +9,7 @@ const Home = () => {
             <Row>
                 <Col sm={12}>
                     <Slider />
+                    <BootstrapTest />
                 </Col>
             </Row>
         </Container>
